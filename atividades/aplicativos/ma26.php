488fa137ac7f4f05579433da5022455f877b1bb4
Marcelo Modesto Costa <marcelomodesto@gmail.com>
2020-03-11 17:37:53 -0300

    corrigidas paginas de aplicativos

--- atividades/aplicativos/ma26.php
+++ atividades/aplicativos/ma26.php
@@ -50,7 +50,7 @@ include($config->cabecalho);
 						<p><strong>Descrição</strong>:</p>
 						<p>Este miniaplicativo auxilia o estudo do modelo Binomial utilizando o software Excel. O usuário escolhe os parâmetros do modelo (n, p), sendo n o número de ensaios (entre 2 e 100) e p a probabilidade de sucesso (entre 0 e 1). O miniaplicativo apresenta o gráfico da função de probabilidade, a média e o desvio-padrão de acordo com os parâmetros escolhidos. Existem, também, sugestões de atividades a serem discutidas com os estudantes. Este miniaplicativo foi adaptado de atividade do portal DIGMATH.</p>
 						<p>&nbsp;</p>
-						<p><a href="/images/arquivos/imagens/ativestat/MA26_Modelo_Binomial_arquivo.xls" target="_blank">Link</a></p>
+						<p><a href="/ativestat/arquivos/MA26_Modelo_Binomial_arquivo.xls" target="_blank">Link</a></p>
 						<p>&nbsp;</p>
 					</section>
 
